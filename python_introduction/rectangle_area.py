38032a94a6f4910ff9adbb2f69f1166906d2c762
Kamunyamike <kamunyawanjohi@gmail.com>
2025-06-01 04:53:12 +0300

    Add a new file rectangle_area.py to python_introduction folder to find area for triangles

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,4 @@
+length = 10 #Value representing the length of the rectangle
+width = 5 #Value representing the width of the rectangle
+area = length * width # The area of the rectangle
+print("The area of the rectangle is: ", area)
