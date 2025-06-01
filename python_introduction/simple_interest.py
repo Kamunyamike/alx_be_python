2934df8dfeb964d2438f5fa61c03c22dda01980a
Kamunyamike <kamunyawanjohi@gmail.com>
2025-06-01 04:43:40 +0300

    Add my_new_file.py to python_introduction folder

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000 # representing $1000
+rate = 0.05 # representing 5%
+time = 3 #representing 3 years
+interest = principal * rate * time
+print ("The simple interest is: ", interest)
