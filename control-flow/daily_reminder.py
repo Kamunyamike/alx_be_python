fba875d4ba3fef257b131b84deacdd3db831bd20
Kamunyamike <kamunyawanjohi@gmail.com>
2025-07-05 16:50:34 +0300

    Add Reminder and Note messages.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,18 +4,18 @@ time_bound = input ("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"{task} is a {priority} priority task that requires immediate attention today!")
+            print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
         else:
-            print(f"{task} is a {priority} priority task. Aim to complete it as soon as possible.")
+            print(f"Reminder: {task} is a {priority} priority task. Aim to complete it as soon as possible.")
     case "medium":
         if time_bound == "yes":
-            print(f"{task} is a {priority} priority task that may need attention soon.")
+            print(f"Reminder: {task} is a {priority} priority task that may need attention soon.")
         else:
-            print(f"{task} is a {priority} priority task. Work on it when your high-priority tasks are done.")
+            print(f"Note: {task} is a {priority} priority task. Work on it when your high-priority tasks are done.")
     case "low":
         if time_bound == "yes":
-            print(f"{task} is a {priority} priority task. Consider completing it when you have free time or during downtime.")
+            print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time or during downtime.")
         else: 
-            print(f"{task} is a {priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
             
             
