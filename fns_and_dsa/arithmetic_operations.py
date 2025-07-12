44a8791334fb4bedd198bf720113bbb392756823
Kamunyamike <kamunyawanjohi@gmail.com>
2025-07-12 12:46:03 +0300

    Changed input variable names from using underscore to combining elements.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
-def perform_operation(num_1,num_2,operation):
+def perform_operation(num1,num2,operation):
     if operation == "add":
-        return num_1 + num_2
+        return num1 + num2
     elif operation == "subtract":
-        return num_1 - num_2
+        return num1 - num2
     elif operation == "multiply":
-        return num_1 * num_2
+        return num1 * num2
     elif operation == "divide":
-        if num_2 == 0:
+        if num2 == 0:
             return "Num_2 cannot have value zero! Put another value other than zero for the denominator."
         else:
-            return num_1 / num_2
+            return num1 / num2
     else: 
         return "Error: Invalid operator. Please use '+', '-', '*', or '/'."
-        
