9c590c3769d8d4c731175dad9373f7ce366ade06
Kamunyamike <kamunyawanjohi@gmail.com>
2025-07-12 17:24:34 +0300

    A python script that converts temperatures between Celsius and Fahrenheit, using global variables to store conversion factors.

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,21 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+temperature = input("Enter the temperature to convert: ")
+state =  input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+if state == "F":
+    fahrenheit = int(temperature)
+    def convert_to_celsius(fahrenheit):
+        global FAHRENHEIT_TO_CELSIUS_FACTOR
+        celsius = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+        return(celsius)
+    print(f"{temperature} F is {convert_to_celsius(fahrenheit)} C")
+elif state == "C":
+        celsius = int(temperature)
+        def convert_to_fahrenheit(celsius):
+            global CELSIUS_TO_FAHRENHEIT_FACTOR
+            fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+            return(fahrenheit)
+        print(f"{temperature} C is {convert_to_fahrenheit(celsius)} F")
+else:
+    print("Invalid temperature. Please enter a numeric value.")
