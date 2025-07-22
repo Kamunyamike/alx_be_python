7345a48b0e80d7c7cec55d5622f1ed5ef63cfb92
Kamunyamike <kamunyawanjohi@gmail.com>
2025-07-22 13:26:51 +0300

    Removed the print statements within my funtions in the bank_account.py file so as to eliminate double result of command functions from the console.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,11 +1,10 @@
 class BankAccount:
-    def __init__(self, initial_balance = 0.0):
+    def __init__(self, initial_balance = 250.0):
         if not isinstance(initial_balance, (int,float)):
             raise TypeError("Initial balance must be a number.")
         if initial_balance < 0:
             raise ValueError("The Initial balance cannot be a negative.")
         self.account_balance = initial_balance
-        print(f"New bank account created with a balance of {self.account_balance}")
     def deposit(self, amount):
         if not isinstance(amount,(int,float)):
             print("Please enter a valid input for your deposits.")
@@ -14,7 +13,7 @@ class BankAccount:
             print("The deposit cannot be a negative figure.")
             return False
         self.account_balance += amount
-        print(f"Deposited: {amount}")
+        
         return True
     def withdraw(self, amount):
         if not isinstance(amount, (int,float)):
@@ -24,10 +23,8 @@ class BankAccount:
             print("You cannot withdraw a negative figure.")
         if self.account_balance >= amount:
             self.account_balance -+ amount
-            print(f"Withdrew: {amount}")
             return True
         else:
-            print("Insufficient funds.")
             return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
