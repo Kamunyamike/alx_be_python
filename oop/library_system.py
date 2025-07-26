8e5e8c77bb2ed44f99932c31db3d9b85804161f6
Kamunyamike <kamunyawanjohi@gmail.com>
2025-07-26 11:01:30 +0300

    Changed the get_info function to the magic function __str__

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,20 +2,20 @@ class Book:
     def __init__(self, title:str, author:str):
         self.title = title
         self.author = author
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
     
 class EBook(Book):
     def __init__(self, title:str, author:str, file_size:int):
         super().__init__(title, author)
         self.file_size = file_size
-    def get_info(self):
+    def __str__(self):
         return f"E-Book: {self.title} by {self.author}, File Size: {self.file_size}KB"
 class PrintBook(Book):
     def __init__(self, title:str, author:str, page_count:int):
         super().__init__(title, author)
         self.page_count = page_count
-    def get_info(self):
+    def __str__(self):
         return f"Print Book: {self.title} by {self.author}, Page Count: {self.page_count}"
     
 class Library:
@@ -26,4 +26,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_info())
+            print(book)
